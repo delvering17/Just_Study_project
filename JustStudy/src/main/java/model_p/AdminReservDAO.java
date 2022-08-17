654df70ef780e9dde312a2f1fcc7059322219d38
jieun <dieunn@naver.com>
2022-08-17 20:39:13 +0900

    Add: 전체 예약 목록, 컬럼별 검색

--- JustStudy/src/main/java/model_p/AdminReservDAO.java
+++ JustStudy/src/main/java/model_p/AdminReservDAO.java
@@ -31,9 +31,9 @@ public class AdminReservDAO {
 
         ArrayList<AdminReservDTO> res = new ArrayList<AdminReservDTO>();
 
-        sql = "SELECT reservation.id, reservation.orderId, reservation.resDate, member.mem_userid, member" +
+        sql = "select reservation.id, reservation.orderId, reservation.resDate, member.mem_userid, member" +
                 ".mem_realname, reservation.city, reservation.branch, reservation.room, reservation.useDate, " +
-                "reservation.TIME, reservation.headcount, reservation.pay, status FROM reservation JOIN member ON " +
+                "reservation.time, reservation.headcount, reservation.pay, status from reservation join member on " +
                 "reservation.userId = member.mem_id";
 
         try {
@@ -68,7 +68,50 @@ public class AdminReservDAO {
 
     }
 
-    public void close() {
+    public ArrayList<AdminReservDTO> listFilter(String filter, String word) {
+
+        ArrayList<AdminReservDTO> res = new ArrayList<AdminReservDTO>();
+
+        sql = "select reservation.id, reservation.orderId, reservation.resDate, member.mem_userid, member" +
+                ".mem_realname, reservation.city, reservation.branch, reservation.room, reservation.useDate, " +
+                "reservation.time, reservation.headcount, reservation.pay, status from reservation join member on " +
+                "reservation.userId = member.mem_id where "+filter+" like ?";
+
+        try {
+            ptmt = con.prepareStatement(sql);
+            ptmt.setString(1, "%"+word+"%");
+            rs = ptmt.executeQuery();
+            while(rs.next()){
+                AdminReservDTO adminReservDTO = new AdminReservDTO();
+
+                adminReservDTO.setId(rs.getInt("id"));
+                adminReservDTO.setOrderId(rs.getString("orderId"));
+                adminReservDTO.setResDate(rs.getTimestamp("resDate"));
+                adminReservDTO.setMem_userid(rs.getString("mem_userid"));
+                adminReservDTO.setMem_realname(rs.getString("mem_realname"));
+                adminReservDTO.setCity(rs.getString("city"));
+                adminReservDTO.setBranch(rs.getString("branch"));
+                adminReservDTO.setRoom(rs.getString("room"));
+                adminReservDTO.setUseDate(rs.getString("useDate"));
+                adminReservDTO.setTime(rs.getString("time"));
+                adminReservDTO.setHeadcount(rs.getInt("headcount"));
+                adminReservDTO.setPay(rs.getInt("pay"));
+                adminReservDTO.setStatus(rs.getString("status"));
+
+                res.add(adminReservDTO);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            close();
+        }
+
+        return res;
+
+    }
+
+
+        public void close() {
         if (rs != null) try {
             rs.close();
         } catch (SQLException e) {

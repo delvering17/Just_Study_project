1cdff510a7b25edf40a2889ea0fd84e4bfd68046
delver <delvering17@gmail.com>
2022-08-17 13:13:54 +0900

    Add: 문의 답변 수정 url 변경

---
+++ JustStudy/src/main/java/admin_p/AdminInquiryModify.java
@@ -0,0 +1,46 @@
+package admin_p;
+
+import model_p.InquiryDAO;
+import model_p.InquiryDTO;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Date;
+
+public class AdminInquiryModify implements AdminService{
+
+
+    @Override
+    public void execute(HttpServletRequest request, HttpServletResponse response) {
+
+
+        String input_title = request.getParameter("input_title");
+        int input_writer = Integer.parseInt(request.getParameter("input_writer"));
+        String input_content = request.getParameter("input_content");
+        int input_purpose = Integer.parseInt(request.getParameter("input_purpose"));
+        int input_id = Integer.parseInt(request.getParameter("input_id"));
+
+        // TODO - 유효성 필요
+
+
+        // 수정
+
+        InquiryDTO inquiryDTO = new InquiryDTO();
+        inquiryDTO.setInquiry_date(new Date());
+        inquiryDTO.setInquiry_title(input_title);
+        inquiryDTO.setInquiry_writer(input_writer);
+        inquiryDTO.setInquiry_content(input_content);
+        inquiryDTO.setInquiry_purpose(input_purpose);
+        inquiryDTO.setInquiry_id(input_id);
+
+        new InquiryDAO().answerModify(inquiryDTO);
+
+        request.setAttribute("msg", "수정되었습니다.");
+
+
+        request.setAttribute("adminUrl","adminInquiryDetailAlert.jsp");
+
+        request.setAttribute("goUrl","AdminInquiryDetail?inquiry_id="+input_purpose);
+
+    }
+}

b67fd128b86fadbbc1b4f38310df2424c896b6ee
jieun <dieunn@naver.com>
2022-08-21 22:51:14 +0900

    Add: faq 페이지 연결

---
+++ JustStudy/src/main/java/customer_p/Faq.java
@@ -0,0 +1,12 @@
+package customer_p;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Faq implements CustomerService{
+    @Override
+    public void execute(HttpServletRequest request, HttpServletResponse response) {
+
+        request.setAttribute("mainUrl", "customer/faq.jsp");
+    }
+}
---
+++ JustStudy/src/main/webapp/view/customer/faq.jsp
@@ -0,0 +1,8 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: dieun
+  Date: 2022-08-21
+  Time: 오후 10:43
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>

5e565c3bac40bb16cb0701e2e5f3d95d7ba1c8a1
delver <delvering17@gmail.com>
2022-08-22 18:05:07 +0900

    Fix: 일반회원으로 로그인하면 관리자로그인 안보이게,관리자 로그인 시 관리자 버튼 보임

--- JustStudy/src/main/java/main_p/MainPage.java
+++ JustStudy/src/main/java/main_p/MainPage.java
@@ -1,11 +1,26 @@
 package main_p;
 
+import model_p.MemberDAO;
+import model_p.MemberDTO;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 public class MainPage implements MainService{
     @Override
     public void execute(HttpServletRequest request, HttpServletResponse response) {
+
+        HttpSession session = request.getSession();
+        Integer mem_id = (Integer) session.getAttribute("login");
+
+
+        if(mem_id != null) {
+            MemberDTO memberDTO = new MemberDAO().detail(mem_id);
+            request.setAttribute("mem_level", memberDTO.getMem_level());
+
+        }
+
 
         request.setAttribute("mainUrl", "main/main.jsp");
     }
--- JustStudy/src/main/webapp/index.jsp
+++ JustStudy/src/main/webapp/index.jsp
@@ -1,6 +1,7 @@
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <%@ page contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" %>
 <script>
-    location.href="board/MainPage"
+            location.href="board/MainPage"
     // location.href="event/EventPage"
     /*location.href="notice/NoticePage"*/
 </script>

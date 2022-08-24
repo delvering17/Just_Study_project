c41afac7b1fa0f20df97f557da5bc32fba2fa467
whay1216 <whay1216@naver.com>
2022-08-24 16:47:05 +0900

    Add: 1:1문의 미답변 갯수 표시, a태그 넘어가면 미답변 문의 출력

---
+++ JustStudy/src/main/java/admin_p/AdminInquiryUnansweredList.java
@@ -0,0 +1,24 @@
+package admin_p;
+
+import model_p.BranchDTO;
+import model_p.InquiryDAO;
+import model_p.InquiryDTO;
+import model_p.ReservationDAO;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+
+public class AdminInquiryList implements AdminService{
+
+    public void execute(HttpServletRequest request, HttpServletResponse response){
+
+
+        ArrayList<InquiryDTO> arr_inquiryDTO = new InquiryDAO().inquiryList();
+
+        request.setAttribute("arr_inquiryDTO", arr_inquiryDTO);
+
+        request.setAttribute("adminUrl","adminInquiryList.jsp");
+    }
+}

f0d4ca5ba3da8e076af13daf350976dd81e6abd6
delver <delvering17@gmail.com>
2022-08-27 13:08:22 +0900

    fix: isBlack() 중복 삭제

--- JustStudy/src/main/java/admin_p/AdminFAQModifyReg.java
+++ JustStudy/src/main/java/admin_p/AdminFAQModifyReg.java
@@ -15,25 +15,19 @@ public class AdminFAQModifyReg implements AdminService{
         String question = request.getParameter("faq-question");
         String answer = request.getParameter("faq-answer");
 
-        if(question==""){
+        if(question.equals("")){
             String msg = "질문을 적어주세요";
             request.setAttribute("adminUrl","alert.jsp");
             request.setAttribute("msg", msg);
             request.setAttribute("goUrl", "AdminFAQModifyDelete?faqId="+faq_id+"&type=수정");
 
-        }else if(answer==""){
+        }else if(answer.equals("")){
             String msg = "답변을 적어주세요";
             request.setAttribute("adminUrl","alert.jsp");
             request.setAttribute("msg", msg);
             request.setAttribute("goUrl", "AdminFAQModifyDelete?faqId="+faq_id+"&type=수정");
 
-        }else if(question.isBlank() || answer.isBlank()) {
-            String msg = "공백으로만 수정할 수 없습니다";
-            request.setAttribute("adminUrl", "alert.jsp");
-            request.setAttribute("msg", msg);
-            request.setAttribute("goUrl", "AdminFAQModifyDelete?faqId="+faq_id+"&type=수정");
-
-        }else{
+        } else {
             FaqDTO faqDTO = new FaqDTO();
 
             faqDTO.setId(Integer.parseInt(request.getParameter("faq-id")));
